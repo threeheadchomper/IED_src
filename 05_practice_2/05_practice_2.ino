6167e14a0a7de19ef50a6eb2680a5456ab1995af
ChoiSungWon <138844513+threeheadchomper@users.noreply.github.com>
2025-09-15 19:55:35 +0900

    Add files via upload

---
+++ 05_practice_2/05_practice_2.ino
@@ -0,0 +1,29 @@
+#define PIN_LED 7
+unsigned int toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  toggle = 1;
+  digitalWrite(PIN_LED, toggle); // turn off LED.
+}
+
+void loop() {
+  toggle = toggle_state(toggle);
+  digitalWrite(PIN_LED, toggle); // turn off LED.
+  delay(1000);
+
+  for(int i=0; i<10; i++)
+  {
+    toggle = toggle_state(toggle);
+    digitalWrite(PIN_LED, toggle); // update LED status.
+    delay(100); // wait for 200 milliseconds
+  }
+
+  toggle = toggle_state(toggle);
+  digitalWrite(PIN_LED, toggle);
+  while(1) {}
+}
+
+int toggle_state(int toggle) {
+  return !toggle;
+}
